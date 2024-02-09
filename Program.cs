7437a91b3ac7caa10d4cf09be85aed0d679bb154
Motestt <159463545+Motestt@users.noreply.github.com>
2024-02-09 18:37:08 -0300

    Add files via upload
    Files

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //linhas e colunas
+
+            Console.Write("linhas:");
+            int N = int.Parse(Console.ReadLine());
+            Console.Write("Colunas");
+            int M = int.Parse(Console.ReadLine());
+
+            //Matriz  
+
+            int[,] mat = new int[N, M];
+
+
+
+            Console.WriteLine("Digite:");
+
+            //Entrada de dados:
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < M; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+
+                }
+
+            }
+
+            //logica
+
+            int z = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] == z)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < N - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < M - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
+
+        }  
+        
+
+          }
+
+                           
+
+}
+    
+
